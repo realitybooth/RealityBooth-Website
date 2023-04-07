39e20efc26f9fd35dcdf09a150dd2b627c994d5e
origamidance <origamidance@gmail.com>
2023-04-07 16:03:48 -0400

    init

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '4fd4c9f2b05f4df38582fe580aecb0bc',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
